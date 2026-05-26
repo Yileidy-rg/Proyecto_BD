efca23eb5a91debd2e178566c3515a7ae16e3f2e
Reychell Acuña <reychell2255@gmail.com>
2026-05-25 22:58:29 -0600

    Corregir api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://backend-bases-de-datos.onrender.com/api/clientes',
+  baseURL: process.env.REACT_APP_API_URL || 'https://backend-bases-de-datos.onrender.com/api',
   timeout: 10000,
 });
 
